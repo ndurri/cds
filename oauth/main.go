d6ff054bc5be8982c3a50837302a0a094e4f7d6b
Neil Durrington <neil@durri.net>
2024-05-24 09:02:10 +0100

    added oauth ui module

--- oauth/main.go
+++ oauth/main.go
@@ -11,6 +11,7 @@ import (
 	"net/http"
 	"regexp"
 	"io"
+	"encoding/json"
 )
 
 var (
@@ -24,6 +25,7 @@ var (
 	TokenHost = mustHave("TOKEN_HOST")
 	TokenBucket = mustHave("TOKEN_BUCKET")
 	TokenPrefix = mustHave("TOKEN_PREFIX")
+	TestUserHost = mustHave("TESTUSER_HOST")
 )
 
 var UUIDRegex = regexp.MustCompile("^[0-9a-f]{8}\\b-[0-9a-f]{4}\\b-[0-9a-f]{4}\\b-[0-9a-f]{4}\\b-[0-9a-f]{12}$")
@@ -36,14 +38,42 @@ func mustHave(varname string) string {
 	return value
 }
 
-func getToken(code string) ([]byte, error) {
-	params := URL.Values{
+type Token struct {
+	AccessToken string `json:"access_token"`
+}
+
+type TestUserSpec struct {
+	ServiceNames []string `json:"serviceNames"`
+	EORI string `json:"eoriNumber"`
+}
+
+func getAccessToken() (*string, error) {
+	tokenJSON, err := tokenForParams(URL.Values{
+		"client_id":     {ClientId},
+		"client_secret": {ClientSecret},
+		"grant_type":    {"client_credentials"},
+	})
+	if err != nil {
+		return nil, err
+	}
+	token := Token{}
+	if err := json.Unmarshal([]byte(tokenJSON), &token); err != nil {
+		return nil, err
+	}
+	return &token.AccessToken, nil
+}
+
+func tokenForCode(code string) ([]byte, error) {
+	return tokenForParams(URL.Values{
 		"client_id":     {ClientId},
 		"client_secret": {ClientSecret},
 		"grant_type":    {"authorization_code"},
 		"redirect_uri":  {RedirectURI},
 		"code":          {code},
-	}
+	})
+}
+
+func tokenForParams(params URL.Values) ([]byte, error) {
 	res, err := http.PostForm(TokenHost, params)
 	if err != nil {
 		return nil, err
@@ -58,6 +88,50 @@ func getToken(code string) ([]byte, error) {
 		return nil, errors.New(errm)
 	}
 	return content, nil
+}
+
+func processTestUser(url string, headers map[string]string, params map[string]string, body string) (int, map[string]string, string) {
+	submitter := params["submitter"]
+	if submitter == "" {
+		fmt.Println(errors.New("submitter not provided"))
+		return 400, nil, ""
+	}
+	spec := TestUserSpec{ServiceNames: []string{"customs-services"}, EORI: submitter}
+	content, err := json.Marshal(spec)
+	if err != nil {
+		fmt.Println(err)
+		return 500, nil, ""
+	}
+	token, err := getAccessToken()
+	if err != nil {
+		fmt.Println(err)
+		return 500, nil, ""
+	}	
+	reader := strings.NewReader(string(content))
+	req, err := http.NewRequest(http.MethodPost, TestUserHost, reader)
+	if err != nil {
+		fmt.Println(err)
+		return 500, nil, ""
+	}
+	req.Header.Add("authorization", "Bearer " + *token)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return 500, nil, ""
+	}
+	defer res.Body.Close()
+	content, err = io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return 500, nil, ""
+	}
+	if res.StatusCode != 201 {
+		fmt.Printf("%s returned %d.", TestUserHost, res.StatusCode)
+		fmt.Println(string(content))
+		return 500, nil, ""		
+	}
+	fmt.Println("oauth: Finished.")
+	return 201, map[string]string{"Content-Type": "application/json"}, string(content)
 }
 
 func processRedirect(url string, headers map[string]string, params map[string]string, body string) (int, map[string]string, string) {
@@ -78,7 +152,7 @@ func processRedirect(url string, headers map[string]string, params map[string]st
 		return 400, nil, "state parameter invalid."
 	}	
 	fmt.Printf("Exchanging code %s for token", code)
-	token, err := getToken(code)
+	token, err := tokenForCode(code)
 	if err != nil {
 		fmt.Println(err)
 		return 500, nil, ""
@@ -117,6 +191,7 @@ func processAuthorize(url string, headers map[string]string, params map[string]s
 		"state": {sessionId},
 	}
 	location.RawQuery = urlparams.Encode()
+	fmt.Println("oauth: Finished.")
 	return 302, map[string]string{"location": location.String()}, ""
 }
 
@@ -125,6 +200,8 @@ func processMessage(url string, headers map[string]string, params map[string]str
 		return processAuthorize(url, headers, params, body)
 	} else if strings.HasPrefix(url, "/oauth/auth-redirect") {
 		return processRedirect(url, headers, params, body)		
+	} else if strings.HasPrefix(url, "/oauth/testuser") {
+		return processTestUser(url, headers, params, body)		
 	}
 	return 404, nil, ""
 }
